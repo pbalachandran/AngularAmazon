20270397e0190ef3b9de7e7c819a728076d719bd
pbalachandran <pbalachandran2014@gmail.com>
2016-12-31 23:36:13 -0600

    Scoped values.

--- src/main/resources/static/app/controllers/SaleSummaryController.js
+++ src/main/resources/static/app/controllers/SaleSummaryController.js
@@ -1,4 +1,15 @@
 angular.module('AmazonApp').controller('SaleSummaryController',
-                                       function($scope, $routeParams) {
+                                       function($scope, $routeParams, InvoiceFactory) {
                                             $scope.invoiceId = $routeParams.invoiceId;
+                                            var promise = InvoiceFactory.retrieveInvoice($scope.invoiceId);
+                                            promise.then(function(data) {
+                                                $scope.totalBeforeTaxes = data.totalBeforeTaxes;
+                                                $scope.taxes = data.taxes;
+                                                $scope.invoiceTotal = data.invoiceTotal;
+                                                $scope.transactionConfirmation = data.transactionConfirmation;
+                                                $scope.date = data.date;
+                                            });
+    
+    
 });
+        
